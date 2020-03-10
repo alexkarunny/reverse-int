b90cf3f199db444d665066737c75e81be15e04c4
Alex Karunny <karunny.alex@gmail.com>
2020-03-10 18:55:55 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let arr = []
+    let arr1 = Math.abs(n).toString().split("");
+  for (let i = arr1.length - 1; i >= 0; i--){
+      arr.push(arr1[i])
+    
+  }
+  let number = +arr.join("");
+  return number;
 }
